ed31e381db55805eb42ca3c3676f452de31b56cb
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2022-05-26 01:02:20 +0530

    Time: 1 ms (96.03%), Space: 44.1 MB (69.73%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String restoreString(String s, int[] indices)
+    {
+        char temp[] = new char[indices.length];
+		for (int i = 0; i < indices.length; i++) 
+        {
+			temp[indices[i]] = s.charAt(i);
+		}
+		return new String(temp); // we can pass character array to string method in java
+    }
+}
