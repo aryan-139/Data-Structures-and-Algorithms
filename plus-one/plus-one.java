b3b4385988b82be9cc7510dd905325757aaed801
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2022-06-04 03:52:43 +0530

    Time: 1 ms, Memory: 42.4 MB - LeetHub

---
+++ plus-one/plus-one.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for(int i = digits.length - 1; i >= 0; i--){
+            if(++digits[i] != 10) return digits;
+            digits[i] = 0;
+        }
+        int [] res = new int[digits.length + 1];
+        res[0] = 1;
+        return res;
+    }
+}
