ede462db8bb5cb34c12f0cea89919deb79935cd6
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2022-06-17 15:40:16 +0530

    Time: 2 ms (69.96%), Space: 41 MB (67.80%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverse(int x) {
+        long sum=0;long n=x;
+        boolean isNegative=false;
+        if(n<0)
+        {isNegative=true;n=-n;}
+        while(n!=0)
+        {
+            sum=sum*10+ n%10;
+            n/=10;
+        }
+       if(sum>Integer.MAX_VALUE)
+        return(0);
+       return (int)(isNegative ? -sum : sum); 
+    }
+}
