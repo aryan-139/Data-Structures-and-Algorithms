3f380a841b8767b00e2a1405a9c4fcac1ac22126
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2022-06-20 09:10:21 +0530

    Time: 25 ms (19.69%), Space: 69.8 MB (43.92%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        //double pointer approach
         int b=0, l=nums.length-1;
         Arrays.sort(nums);
         for(int i=0;i<nums.length-1;i++)
