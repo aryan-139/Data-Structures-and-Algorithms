681b8c2a388f6b28b4a8f6c4740905085d7ea460
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2022-06-21 20:10:40 +0530

    Time: 1 ms (73.10%), Space: 44.3 MB (36.20%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
@@ -8,58 +8,31 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-// class Solution {
-//     public ListNode deleteDuplicates(ListNode head) {
-//         ListNode unique=head;
-//         if(head.next==null)
-//             return(head);
-//         ListNode curr= head;
-        
-//         while(curr!=null){
-//             curr=curr.next;
-//             if(curr==null)
-//                 break;
-//             if(curr.val==head.val){
-//                 continue;
-//             }
-//             else
-//             {
-//                 head.next=curr;
-//             }
-            
-//            head=head.next;
-//         }
-//         return(head);
-//     }
-// }
 class Solution {
+
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode unique=head;
+        ListNode unique = head;
         // if(head.next==null)
         //     return(head);
-        ListNode curr= head;
-        
-        while(curr!=null){
-            if(curr.next==null)
-            {
+        ListNode curr = head;
+
+        while (curr != null) {
+            if (curr.next == null) {
                 //to check the duplicates in the last 2 position
-                if(curr.val==unique.val)
-                {unique.next=null; break;}
-                else
+                if (curr.val == unique.val) {
+                    unique.next = null;
                     break;
+                } else break;
             }
-            curr=curr.next;
-            
-            if(curr.val==unique.val){
+            curr = curr.next;
+
+            if (curr.val == unique.val) {
                 continue;
-            }
-            else
-                unique.next=curr;
-            
-            
-           unique=unique.next;
+            } else unique.next = curr;
+
+            unique = unique.next;
         }
-        
-        return(head);
+
+        return (head);
     }
 }
