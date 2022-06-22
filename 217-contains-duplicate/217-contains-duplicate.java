6c51abed7dafb47af34c576e32bf60a311af1c1c
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2022-06-23 00:06:49 +0530

    Time: 38 ms (5.44%), Space: 71.6 MB (17.32%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,20 +1,35 @@
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         //double pointer approach
+//         int b=0, l=nums.length-1;
+//         Arrays.sort(nums);
+//         for(int i=0;i<nums.length-1;i++)
+//         {
+//             if(nums[b]==nums[b+1])
+//                 return(true);
+//             else
+//                 b++;
+//             if(nums[l]==nums[l-1])
+//                 return(true);
+//             else
+//                 l--;
+//             if(l==b)
+//                 break;
+//         }
+//         return(false);
+//     }
+// }
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        //double pointer approach
-        int b=0, l=nums.length-1;
+        //neighbouring element approach
+        
         Arrays.sort(nums);
         for(int i=0;i<nums.length-1;i++)
         {
-            if(nums[b]==nums[b+1])
+            if(nums[i]==nums[i+1])
                 return(true);
             else
-                b++;
-            if(nums[l]==nums[l-1])
-                return(true);
-            else
-                l--;
-            if(l==b)
-                break;
+                continue;
         }
         return(false);
     }
