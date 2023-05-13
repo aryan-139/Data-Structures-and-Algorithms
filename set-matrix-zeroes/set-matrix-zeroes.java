2a6a57592d8bb35579bcf187fb40871b540c29b2
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2023-05-13 17:02:55 +0530

    Time: 1 ms (71.78%) | Memory: 44 MB (66.93%) - LeetSync

---
+++ set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        Set<Integer> rows = new HashSet<>();
+        Set<Integer> cols = new HashSet<>();
+
+        // Traverse the matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.add(i);
+                    cols.add(j);
+                }
+            }
+        }
+
+        // Clear the marked rows
+        for (int row : rows) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[row][j] = 0;
+            }
+        }
+
+        // Clear the marked columns
+        for (int col : cols) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][col] = 0;
+            }
+        }
+    }
+}
