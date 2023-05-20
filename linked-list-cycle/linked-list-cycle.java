4651be4586a933b47e9c4a006496f317dbf68818
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2023-05-20 21:11:20 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (31.63%) - LeetSync

--- linked-list-cycle/linked-list-cycle.java
+++ linked-list-cycle/linked-list-cycle.java
@@ -11,19 +11,36 @@ import java.util.HashSet;
  *     }
  * }
  */
+// import java.util.HashSet;
+
+// public class Solution {
+//     public boolean hasCycle(ListNode head) {
+//         HashSet<ListNode> cycleDetection = new HashSet<>();
+//         ListNode curr = head;
+//         while (curr != null) {
+//             if (cycleDetection.contains(curr)) {
+//                 return true;
+//             }
+//             cycleDetection.add(curr);
+//             curr = curr.next;
+//         }
+//         return false;
+//     }
+// }
+
 import java.util.HashSet;
 
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        HashSet<ListNode> cycleDetection = new HashSet<>();
-        ListNode curr = head;
-        while (curr != null) {
-            if (cycleDetection.contains(curr)) {
-                return true;
-            }
-            cycleDetection.add(curr);
-            curr = curr.next;
-        }
-        return false;
+        if(head == null) return false;
+    ListNode fast = head;
+    ListNode slow = head;
+        
+    while(fast.next != null && fast.next.next != null) {
+        fast = fast.next.next;
+        slow = slow.next;
+        if(fast == slow) return true;
+    }
+    return false;
     }
 }
