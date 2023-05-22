0e93e77a7be9eef84db97deae4efb23a9c2b478d
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2023-05-22 18:55:52 +0530

    Time: 43 ms (64.27%) | Memory: 51.2 MB (7.41%) - LeetSync

---
+++ 3sum/3sum.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> fin = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int target = -nums[i];
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+
+                if (sum == target) {
+                    List<Integer> currTriplet = new ArrayList<>();
+                    currTriplet.add(nums[i]);
+                    currTriplet.add(nums[left]);
+                    currTriplet.add(nums[right]);
+                    fin.add(currTriplet);
+
+                    // Skip duplicates
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+
+                    // Move pointers
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return fin;
+    }
+}
