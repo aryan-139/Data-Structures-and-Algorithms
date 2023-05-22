b5492ead179dee33bf9a021d51a367b0dba204e9
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2023-05-22 14:28:29 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (99.13%) - LeetSync

--- rotate-list/rotate-list.java
+++ rotate-list/rotate-list.java
@@ -3,6 +3,7 @@ class Solution {
         if (head == null || head.next == null || k == 0) {
             return head;
         }
+        //handling all the edge cases 
 
         int length = 1;
         ListNode tail = head;
@@ -10,26 +11,29 @@ class Solution {
             tail = tail.next;
             length++;
         }
-
+        //if there are nx rotations then handle that edge case 
         k = k % length;
         if (k == 0) {
             return head;
         }
-
+        //now only when all the edge cases are tackled should we do something completely new
+        //that is rotate the linked list 
         ListNode slow = head;
         ListNode fast = head;
         for (int i = 0; i < k; i++) {
             fast = fast.next;
         }
-
+        //move the fast pointer ahead appropriately
         while (fast.next != null) {
             slow = slow.next;
             fast = fast.next;
         }
+        //just traverse till we get fast as null 
 
         ListNode newHead = slow.next;
         slow.next = null;
         tail.next = head;
+        //just modifying the break and addition points 
         return newHead;
     }
 }
