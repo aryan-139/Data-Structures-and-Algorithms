c78d4fb05bffb239f1c6a1218ff485825030b3f2
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2023-06-02 18:22:00 +0530

    Time: 1 ms (94.11%) | Memory: 43.6 MB (18.23%) - LeetSync

--- generate-parentheses/generate-parentheses.java
+++ generate-parentheses/generate-parentheses.java
@@ -4,11 +4,15 @@ import java.util.List;
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
+        //first i am simply creating the result arraylist
         backtrack("", 0, 0, n, result);
+        //then a backtrack function is created which keeps the tab of the total number of open and closed parenthesis
+        //our objective is that the total number of closed parenthesis at any point of time must be less than that of open and the total number of open parenthesis must be less than the max that it can containˀˀ
         return result;
     }
 
     private void backtrack(String current, int open, int close, int max, List<String> result) {
+        //base case
         if (current.length() == max * 2) {
             result.add(current);
             return;
