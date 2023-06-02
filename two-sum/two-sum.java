f4a400cd5f74b4c6f07824a8dec259118e01f342
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2023-06-02 20:59:42 +0530

    Time: 1 ms (99.43%) | Memory: 43.8 MB (20.00%) - LeetSync

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,36 +1,18 @@
-import java.util.Scanner;
-//finding twosum
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int res[]=new int[2];
-        for(int i=0;i<nums.length;i++)
-        {
-            for(int j=i+1;j<nums.length;j++)
-            if(nums[i]+nums[j]==target)
-                {
-                    res[0]=i;
-                    res[1]=j;
-                    return(res);
-                }
-                else
-                continue;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
         }
-        res[0]=0;
-        res[1]=0;
-        return(res);
-    }
 
-    public static void main(String args[]) {
-        Scanner sc=new Scanner(System.in);
-        Solution obj=new Solution();
-        int len=sc.nextInt();
-        int nums[]=new int[len]; int target;
-        for(int i=0;i<len;i++)
-            nums[i]=sc.nextInt();
-        System.out.println("Enter the number for the target array");
-        target=sc.nextInt();
-        int[] res=obj.twoSum(nums,target);
-        
+        throw new IllegalArgumentException("No two elements add up to the target.");
     }
-
 }
