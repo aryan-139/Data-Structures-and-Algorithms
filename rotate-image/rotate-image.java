192c01643e6ff8873a058f4da511adeefc2e9044
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2023-06-02 20:21:39 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (98.06%) - LeetSync

---
+++ rotate-image/rotate-image.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int temp;
+        //since we have to rotate the matrix and not take another array, we can do substitutions instead 
+        // the idea here is to transpose the matrix first then flip it 
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=i;j<matrix.length;j++)
+            {
+                temp=0;
+                temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        //flipping the matrix
+        for(int m=0;m<matrix.length;m++)
+        {
+            for(int n=0;n<matrix.length/2;n++)
+            {
+                temp=0;
+                temp=matrix[m][n];
+                matrix[m][n]=matrix[m][matrix.length-1-n];
+                matrix[m][matrix.length-1-n]=temp;
+            }
+        }
+        
+    }
+}
