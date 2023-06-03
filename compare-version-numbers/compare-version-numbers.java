09e717760c4635cfd555405de8f7a2b7f4211e58
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2023-06-03 21:50:37 +0530

    Time: 1 ms (85.44%) | Memory: 40.6 MB (65.80%) - LeetSync

--- compare-version-numbers/compare-version-numbers.java
+++ compare-version-numbers/compare-version-numbers.java
@@ -1,10 +1,12 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
+        //simply creating two arrays which contain the version number which is split using the regex of \\.
     String[] levels1 = version1.split("\\.");
     String[] levels2 = version2.split("\\.");
-    
+    //run through the maximum length of levels
     int length = Math.max(levels1.length, levels2.length);
     for (int i = 0; i < length; i++) {
+        //keeep comparing the v1 and v2 that is the version numbers and if at any point it is less or greater then return, if equal then continue
         Integer v1 = i < levels1.length ? Integer.parseInt(levels1[i]) : 0;
         Integer v2 = i < levels2.length ? Integer.parseInt(levels2[i]) : 0;
         int compare = v1.compareTo(v2);
