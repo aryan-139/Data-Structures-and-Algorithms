297af9de60971bd690085a28a8982e3b75187853
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2023-06-03 21:28:06 +0530

    Time: 19 ms (21.31%) | Memory: 44 MB (6.31%) - LeetSync

--- valid-anagram/valid-anagram.java
+++ valid-anagram/valid-anagram.java
@@ -1,17 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length())
-            return(false);
-        int m[]= new int[s.length()];
-        int n[]=new int[t.length()];
-        for(int i=0;i<m.length;i++){
-            m[i]=(int)s.charAt(i); n[i]=(int)t.charAt(i);
+        if (s.length() != t.length()) {
+            return false;
         }
-        Arrays.sort(m); Arrays.sort(n);
-        if(Arrays.equals(m,n))
-            return(true);
-        else
-            return(false);
-        
+
+        Map<Character, Integer> charCount = new HashMap<>();
+
+        // Count characters in string s
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+
+        // Decrement character count for string t
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            if (!charCount.containsKey(c) || charCount.get(c) == 0) {
+                return false;
+            }
+            charCount.put(c, charCount.get(c) - 1);
+        }
+
+        return true;
     }
 }
