0bf26fc6ed6fb5fe40fdb7634fc8c2fe49292d4e
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2023-06-25 22:05:08 +0530

    Time: 465 ms (11.88%) | Memory: 95.1 MB (14.03%) - LeetSync

--- longest-increasing-subsequence/longest-increasing-subsequence.java
+++ longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,18 +1,39 @@
+import java.util.*;
+
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1); // Initialize all LIS lengths to 1
-        
-        int maxLen = 1; // Minimum LIS length is 1
-        
-        for (int i = 1; i < nums.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                    maxLen = Math.max(maxLen, dp[i]);
-                }
-            }
+        // Create a memoization cache
+        int[][] cache = new int[nums.length][nums.length + 1];
+        for (int[] row : cache) {
+            Arrays.fill(row, -1);
         }
+        
+        return lisHelper(nums, -1, 0, cache);
+    }
+    
+    private int lisHelper(int[] nums, int prevIndex, int currentIndex, int[][] cache) {
+        if (currentIndex == nums.length) {
+            return 0; // Reached the end of the array
+        }
+        
+        if (cache[prevIndex + 1][currentIndex] != -1) {
+            return cache[prevIndex + 1][currentIndex]; // Return memoized result if available
+        }
+        
+        int taken = 0;
+        if (prevIndex == -1 || nums[currentIndex] > nums[prevIndex]) {
+            // Include the current element in the LIS
+            taken = 1 + lisHelper(nums, currentIndex, currentIndex + 1, cache);
+        }
+        
+        // Exclude the current element from the LIS
+        int notTaken = lisHelper(nums, prevIndex, currentIndex + 1, cache);
+        
+        // Calculate the maximum LIS length at the current index
+        int maxLen = Math.max(taken, notTaken);
+        
+        // Store the result in the cache
+        cache[prevIndex + 1][currentIndex] = maxLen;
         
         return maxLen;
     }
