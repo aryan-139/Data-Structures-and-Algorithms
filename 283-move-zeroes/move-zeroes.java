e81a7d164f22a2851c8e2b9d3b492245a8ed2973
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2023-08-04 00:46:06 +0530

    Time: 2 ms (42.49%) | Memory: 44.9 MB (87.61%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,16 +1,20 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                for(int j=i;j<nums.length;j++){
-                    if(nums[j]!=0){
-                        nums[i]=nums[j];
-                        nums[j]=0;
-                        break;
-                    }
-                }
-                
+        int left = 0; // Points to the position to insert non-zero elements
+        int right = 0; // Traverses the array to find non-zero elements
+
+        while (right < nums.length) {
+            if (nums[right] != 0) {
+                nums[left] = nums[right];
+                left++;
             }
+            right++;
+        }
+
+        // Fill the rest of the array with zeroes
+        while (left < nums.length) {
+            nums[left] = 0;
+            left++;
         }
     }
 }
