8486935d257251931fdf6999df5f0f13a1530f29
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2023-08-06 19:19:23 +0530

    Time: 200 ms (41.54%) | Memory: 47.4 MB (25.09%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+import java.util.Stack;
+
+class Solution {
+    public String removeStars(String s) {
+        Stack<Character> backspace=new Stack<>();
+        //add to stack 
+        for(char i:s.toCharArray()){
+            backspace.add(i);
+        }
+        StringBuilder res=new StringBuilder();
+        //check for stars 
+        int countStar=0;
+        while(!backspace.isEmpty()){
+            if(backspace.peek()=='*'){
+                countStar++;
+                backspace.pop();
+            }
+            else if((countStar!=0)&&(Character.isLetter(backspace.peek()))){
+                backspace.pop();
+                countStar--;
+            }
+            else{
+                res.append(backspace.peek());
+                backspace.pop();
+            }
+        }
+        return res.reverse().toString();
+
+    }
+}
