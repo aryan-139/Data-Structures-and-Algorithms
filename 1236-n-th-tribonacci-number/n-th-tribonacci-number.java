d876b5d8cbdbdd510e3b6d001f75c5eb6bdd01b6
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2023-08-25 15:25:51 +0530

    Time: 0 ms (100.00%) | Memory: 39.2 MB (50.62%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n==0||n==1)
+            return n;
+        int memo[]=new int[n+1];
+        memo[0]=0;
+        memo[1]=1;
+        memo[2]=1;
+        for(int i=3;i<=n;i++){
+            memo[i]=memo[i-1]+memo[i-2]+memo[i-3];
+        }
+        return memo[n];
+        }
+    }
