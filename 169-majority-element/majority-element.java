e42dc9a159209a6428707664d4ef9a044d16bb31
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2023-08-27 15:41:50 +0530

    Time: 5 ms (40.51%) | Memory: 48.3 MB (59.27%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,17 +1,40 @@
 class Solution {
     public int majorityElement(int[] nums) {
-       HashMap<Integer, Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
-        }
-        int max=0,val=0;
-        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-            if(entry.getValue()>max){
-                max=entry.getValue();
-                val=entry.getKey();
-            }
+        //Method 1
+    //    HashMap<Integer, Integer> map=new HashMap<>();
+    //     for(int i=0;i<nums.length;i++){
+    //         map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+    //     }
+    //     int max=0,val=0;
+    //     for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+    //         if(entry.getValue()>max){
+    //             max=entry.getValue();
+    //             val=entry.getKey();
+    //         }
             
+    //     }
+    //     return val;
+    //Method 2 
+
+    Arrays.sort(nums);
+    int max=0,c=0;
+    int curr=nums[0];
+    for(int i=0;i<nums.length;i++){
+        if(nums[i]==curr){
+            c++;
+        } else{
+            if(c>nums.length/2)
+            return curr;
+            if(c>max)
+            max=c;
+            curr=nums[i];
+            c=1;
         }
-        return val;
+    }
+     if (c > nums.length / 2) {
+            return curr;  // Check for the last element
+        }
+    return -1;
+
     }
 }
