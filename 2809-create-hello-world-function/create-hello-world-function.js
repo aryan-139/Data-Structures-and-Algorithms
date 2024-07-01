5a8f62843e81726f0c520ac8619d10bf50766a20
Aryan Raj <aryanraj2k25@gmail.com>
2024-07-01 20:12:44 +0100

    Time: 55 ms (30.04%) | Memory: 48.9 MB (10.53%) - LeetSync

--- 2809-create-hello-world-function/create-hello-world-function.js
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -1,14 +1,5 @@
-/**
- * @return {Function}
- */
-var createHelloWorld = function() {
-    // return ()=>"Hello World";
+var createHelloWorld= function(){
     return function(...args){
-        return("Hello World");
+        return "Hello World";
     }
 };
-
-/**
- * const f = createHelloWorld();
- * f(); // "Hello World"
- */
