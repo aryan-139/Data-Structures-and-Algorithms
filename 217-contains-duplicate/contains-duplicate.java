63a4007739b2af9c495879f59a69cadb8ede9c99
Aryan Raj <aryanraj2k25@gmail.com>
2024-09-16 19:55:22 +0530

    Time: 10 ms (89.52%) | Memory: 58.1 MB (40.94%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,14 +1,13 @@
 import java.util.HashSet;
-import java.util.Arrays;
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        // No need to check if length < 2, it won't contain duplicates
-        for(int i = 1; i < nums.length; i++) {
-            if(nums[i] == nums[i-1]) {  // Check if consecutive numbers are the same
-                return true;
+        HashSet <Integer> set = new HashSet<>();
+        for(int num : nums) {
+            if(set.contains(num)) {
+                return true;  
             }
+            set.add(num);
         }
         return false;
     }
