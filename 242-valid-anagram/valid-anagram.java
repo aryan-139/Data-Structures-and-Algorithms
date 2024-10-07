87368cf6a0b73478d839452bfb6064ece59df581
Aryan Raj <aryanraj2k25@gmail.com>
2024-10-07 22:37:49 +0530

    Time: 27 ms (5.52%) | Memory: 44.2 MB (58.14%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,15 +1,39 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-        int[] store = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            store[s.charAt(i) - 'a']++;
-            store[t.charAt(i) - 'a']--;
-            //incrementing and decrementing to create a good check statement
-        }
-        for (int n : store) 
-        if (n != 0) 
+        //base case
+        if(s.length()!=t.length())
         return false;
+
+        //edge case 
+        if((s.length()==0)&&(t.length()==0))
         return true;
+
+        HashMap <Character, Integer> map=new HashMap<>();
+        HashMap <Character, Integer> map2=new HashMap<>();
+
+        //add characters to the HashMap 
+        for(int i=0;i<s.length();i++){
+            char ch =s.charAt(i);
+            char ch2 =t.charAt(i);
+
+            //add the characters to Map 1
+            if(map.containsKey(ch)){
+                map.put(ch, map.get(ch)+1);
+            } else{
+                map.put(ch, 1);
+            }
+
+            //add the characters to Map2 
+            if(map2.containsKey(ch2)){
+                map2.put(ch2, map2.get(ch2)+1);
+            }else{
+                map2.put(ch2, 1);
+            }
+        }
+        //compare the 2 HashMaps
+        if(map.equals(map2))
+        return true;
+        else
+        return false;
     }
 }
