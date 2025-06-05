a465e4391b3c38dc281385a556cbfc7503ca2431
Aryan Raj <aryanraj2k25@gmail.com>
2025-06-06 00:23:19 +0530

    Time: 1 ms (79.17%) | Memory: 46.1 MB (26.31%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int profit = 0;
-
+        //whichever day profit, collect it 
         for (int i = 1; i < prices.length; i++) {
             // Accumulate profit on every upward slope
             if (prices[i] > prices[i - 1]) {
