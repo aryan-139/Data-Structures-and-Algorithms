1cf0316d4f184b2811d111a338af5a52c408a849
Aryan Raj <aryanraj2k25@gmail.com>
2025-06-11 21:16:34 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (40.20%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,20 +1,25 @@
-class Solution {
+public class Solution {
+    private int[][] memo;
+    
     public int rob(int[] nums) {
         if (nums.length == 1) return nums[0];
-
-        int max1 = robLinear(nums, 0, nums.length - 2);
-        int max2 = robLinear(nums, 1, nums.length - 1);
-
-        return Math.max(max1, max2);
-    }
-
-    private int robLinear(int[] nums, int start, int end) {
-        int prev = 0, curr = 0;
-        for (int i = start; i <= end; i++) {
-            int temp = Math.max(curr, prev + nums[i]);
-            prev = curr;
-            curr = temp;
+        
+        memo = new int[nums.length][2];
+        for (int i = 0; i < nums.length; i++) {
+            memo[i][0] = -1;
+            memo[i][1] = -1;
         }
-        return curr;
+        
+        return Math.max(dfs(0, 1, nums), dfs(1, 0, nums));
+    }
+    
+    private int dfs(int i, int flag, int[] nums) {
+        if (i >= nums.length || (flag == 1 && i == nums.length - 1)) 
+            return 0;
+        if (memo[i][flag] != -1)
+            return memo[i][flag];
+        memo[i][flag] = Math.max(dfs(i + 1, flag, nums), 
+                        nums[i] + dfs(i + 2, flag | (i == 0 ? 1 : 0), nums));
+        return memo[i][flag];
     }
 }
