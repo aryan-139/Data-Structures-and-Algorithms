359fb160d032efd1e3bab500c15b8eb8afbfd67e
Aryan Raj <aryanraj2k25@gmail.com>
2025-06-19 10:00:29 +0530

    Time: 2 ms (85.00%) | Memory: 52 MB (96.90%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
@@ -1,38 +1,13 @@
-
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    int count=0;
     public int goodNodes(TreeNode root) {
-        TreeNode currVal=root;
-        TreeNode rootCopy=root;
-        dfsUtil(root,currVal,rootCopy);
-          System.out.println(count);
-        return count;
+        return dfs(root, root.val);
     }
-    private void dfsUtil(TreeNode root, TreeNode currVal, TreeNode rootCopy){
-        if(root==null)
-        return;
-        if(root.val >=currVal.val){
-            count++;
-            currVal=root;
-        }
-      
-        dfsUtil(root.left, currVal, rootCopy);
-        dfsUtil(root.right, currVal, rootCopy);
-        // currVal=rootCopy;
+
+    public int dfs(TreeNode root, int pathHighest) {
+        if (root == null) return 0;
+        int good = 0;
+        if (root.val >= pathHighest) good = 1;
+        int newPathHighest = Math.max(pathHighest, root.val);
+        return good + dfs(root.left, newPathHighest) + dfs(root.right, newPathHighest);
     }
 }
