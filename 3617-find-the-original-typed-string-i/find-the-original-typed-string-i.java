3fea5353b0c7050e7f22d5d31a2561733b2f4a12
Aryan Raj <aryanraj2k25@gmail.com>
2025-07-02 01:16:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (38.71%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.java
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.java
@@ -1,20 +1,10 @@
 class Solution {
     public int possibleStringCount(String word) {
-        int count = 1; // original string is always valid
-        int i = 0;
-
-        while (i < word.length()) {
-            int j = i;
-            while (j < word.length() && word.charAt(j) == word.charAt(i)) {
-                j++;
-            }
-            int len = j - i; // length of current group
-            if (len > 1) {
-                count += (len - 1); // how many smaller variants for this group
-            }
-            i = j;
+        int ans=1; 
+        for(int i=1; i<word.length();i++){
+            if(word.charAt(i)==word.charAt(i-1))
+            ans++;
         }
-
-        return count;
+        return ans;
     }
 }
