9b188a8ea9015b762c5e8a55d04ad4e5e830f3b7
Aryan Raj <aryanraj2k25@gmail.com>
2025-07-02 23:40:07 +0530

    Time: 4 ms (51.29%) | Memory: 48.7 MB (97.13%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,25 +1,30 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int count=0; 
+        int vis[][]= new int[grid.length][grid[0].length];
+        for(int i=0; i<vis.length; i++){
+            for(int j=0; j<vis[0].length; j++){
+                vis[i][j]=-1;
+            }
+        }
+        int count=0;
         for(int i=0; i<grid.length; i++){
             for(int j=0; j<grid[0].length; j++){
-                //look for 1 and fill the matrix with V everywhere where it is a connected component
-                if(grid[i][j]=='1'){
-                    dfs(grid, i, j); 
-                    count+=1; 
+                if (grid[i][j] == '1' && vis[i][j] == -1) {
+                    dfs(grid, vis, i, j);
+                    count++;
                 }
             }
         }
-        return count; 
+        return count;
     }
-
-    public void dfs(char m[][], int i, int j){
-        if(i<0 || j<0 || i>=m.length || j>=m[0].length || m[i][j]!='1') 
-        return; 
-        m[i][j]='V';
-        dfs(m, i+1, j);
-        dfs(m, i-1, j);
-        dfs(m, i, j+1);
-        dfs(m, i, j-1);
+    private void dfs(char [][] grid, int[][] vis, int i, int j){
+        int rows=grid.length; int cols=grid[0].length;
+        if(i<0 || j<0 || i>=rows || j>=cols) return; 
+        if(grid[i][j] == '0' || vis[i][j]!=-1) return; 
+        vis[i][j]=1; 
+        dfs(grid, vis, i+1, j);
+        dfs(grid, vis, i-1, j);
+        dfs(grid, vis, i, j+1);
+        dfs(grid, vis, i, j-1);
     }
 }
