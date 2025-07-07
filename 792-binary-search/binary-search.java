e97c2aa9ca6b15a5a595ac6fe4ebff29f004c01e
Aryan Raj <aryanraj2k25@gmail.com>
2025-07-07 19:27:01 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (60.51%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -5,15 +5,12 @@ class Solution {
         return 0; 
         while(lo<=hi){
             mid = lo + (hi - lo) / 2;
-            System.out.println("mid"+mid);
             if(nums[mid]==target){
                 return mid;
             }else if(nums[mid]<target){
                 lo=mid+1;
-                System.out.println("lo"+lo);
             }else if(nums[mid]>target){
                 hi=mid-1;
-                System.out.println("hi"+hi);
             }
         }
         return -1; 
