7776958359c0c0068556a568dcfe22db413c6406
Aryan Raj <aryanraj2k25@gmail.com>
2025-07-10 00:57:24 +0530

    Time: 2 ms (96.80%) | Memory: 41.7 MB (65.74%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean isValid(String s) {
-        HashMap<Character, Character> map= new HashMap<>();
-        Stack<Character> st= new Stack<>();
-        map.put('{','}');
-        map.put('[',']');
-        map.put('(',')');
-        //now iterate the entire string 
-        for(int i=0; i<s.length();i++){
-            char ch=s.charAt(i);
-            if(map.containsKey(ch)){
-                st.add(ch);
-            }else{
-                if (st.isEmpty() || map.get(st.pop()) != ch) {
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                if (st.isEmpty()) return false;
+                char top = st.pop();
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
                     return false;
                 }
             }
