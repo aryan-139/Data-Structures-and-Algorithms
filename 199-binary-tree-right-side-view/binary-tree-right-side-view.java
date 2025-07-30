90224eb7d158e07ff81ad6d33666fbe3d8ff279f
Aryan Raj <aryanraj2k25@gmail.com>
2025-07-31 00:06:44 +0530

    Time: 1 ms (71.00%) | Memory: 42.5 MB (15.72%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,56 +1,29 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 class Solution {
-    List<Integer> ans = new ArrayList<>();
-    
     public List<Integer> rightSideView(TreeNode root) {
-        if (root == null)
-            return ans;
-        rightViewHelper(root);
-        return ans;
-    }
-    
-    private void rightViewHelper(TreeNode root) {
-        if (root == null)
-            return;
-        
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+
         Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        
+        q.offer(root);
+
         while (!q.isEmpty()) {
             int levelSize = q.size();
-            
+            TreeNode rightMost = null;
+
             for (int i = 0; i < levelSize; i++) {
-                TreeNode node = q.poll();
-                
-                if (i == levelSize - 1) { // If it's the last node in this level, add to answer
-                    ans.add(node.val);
-                }
-                
-                if (node.left != null) {
-                    q.add(node.left);
-                }
-                
-                if (node.right != null) {
-                    q.add(node.right);
-                }
+                TreeNode current = q.poll();
+
+                // The last node encountered in this level will be the rightmost
+                rightMost = current;
+
+                if (current.left != null) q.offer(current.left);
+                if (current.right != null) q.offer(current.right);
             }
+
+            // After the level, add the rightmost node's value
+            result.add(rightMost.val);
         }
+
+        return result;
     }
 }
