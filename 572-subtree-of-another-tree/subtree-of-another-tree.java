d99bc2c5a51d389f77c599af1f1f0f0d3bf84810
Aryan Raj <aryanraj2k25@gmail.com>
2026-01-06 22:39:18 +0530

    Time: 2 ms (99.10%) | Memory: 46.6 MB (15.75%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -1,17 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if (root == null) return false;
-
-        if (isSimilar(root, subRoot)) return true;
-
+        if(root==null) return false; 
+        if(isSame(root, subRoot)) return true; 
         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
-
-    public boolean isSimilar(TreeNode p, TreeNode q) {
-        if (p == null && q == null) return true;
-        if (p == null || q == null) return false;
-        if (p.val != q.val) return false;
-
-        return isSimilar(p.left, q.left) && isSimilar(p.right, q.right);
+    private boolean isSame(TreeNode root, TreeNode subRoot){
+        if(root==null && subRoot==null) return true; 
+        if(root==null || subRoot==null) return false;
+        if(root.val!= subRoot.val) return false;  
+        return isSame(root.left, subRoot.left) && isSame(root.right, subRoot.right); 
     }
 }
