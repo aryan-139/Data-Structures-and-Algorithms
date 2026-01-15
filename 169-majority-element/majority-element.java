baa6dd3ac26d3207984b8cb06ffc2270cdccd9b3
Aryan Raj <aryanraj2k25@gmail.com>
2026-01-15 15:01:39 +0530

    Time: 348 ms (7.32%) | Memory: 56.7 MB (10.11%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,17 +1,30 @@
 class Solution {
-    //we can use hashing someway
     public int majorityElement(int[] nums) {
-        //hashmap with <key, count> 
-        HashMap<Integer, Integer> map= new HashMap<>();
-        for(int num: nums){
-            map.put(num, map.getOrDefault(num,0) +1 );
-        }
-        //traverse hashmap return key where count > n/2
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > nums.length / 2) {
-                return entry.getKey(); 
+        int ve=nums[0];
+        int c=1; 
+        //moore's voting algorithm O(n)
+        for(int i=1; i<nums.length-1; i++){
+            if(nums[i]==ve){
+                c++;
+                System.out.println(i+" "+ve+" "+c);
             }
+            else {
+                --c;
+                System.out.println(i+" "+ve+" "+c);
+            }
+
+            if(c==0){
+                System.out.println(i+" "+ve);
+                ve=nums[i+1];
+            }
+
         }
-        return -1;
+        System.out.print(ve);
+        return ve;
+        //O(n) to determine which one has > n/2
+
+
+
+
     }
 }
